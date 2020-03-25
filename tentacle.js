0a830826b26d806f743d12eb641ccf23ea120aaf
Rob Kayson <rkayson@gmail.com>
2020-03-24 21:52:05 -0700

    Worm

--- tentacle.js
+++ tentacle.js
@@ -2,29 +2,31 @@ let frame = 0
 let i
 // const rings = []
 let dir = 0
+// let frontseed = random(Date.now())
+// let backseed = random(Date.now())
 
 function drawTentacle(x, y, d, d2, seed) {
     strokeWeight(3)
     stroke(254,1,90, 130)
     fill(255)
     i = 0
+    let backside = []
     let _x = x
     let _y = y
-    let _d = d2
+    let _d = d
     let _dir = dir
     const length = 3
 
-    let backside = []
-    while (_d < d) {
-        i++
-        _dir += (noise(i, frame) - 0.5) * v.nuzz
-        _x = _x + cos(_dir) * length//* (i+1) 
-        _y = _y + sin(_dir) * length//* (i+1) 
-        circle(_x, _y, _d)
-        _d += 1
-    }
+    // while (_d < d) {
+    //     i++
+    //     _dir += (noise(i, frame) - 0.5) * v.nuzz
+    //     _x = _x + cos(_dir) * length//* (i+1) 
+    //     _y = _y + sin(_dir) * length//* (i+1) 
+    //     circle(_x, _y, _d)
+    //     _d += 1
+    // }
 
-    // while (d > d2) {
+    while (_d > d2) {
         // let ring = rings[i++]
         // if (!ring) {
         //     ring = {
@@ -36,13 +38,34 @@ function drawTentacle(x, y, d, d2, seed) {
         //     rings.push(ring)
         // }
 
-        // i++
-        // _dir += (noise(i, frame) - 0.5) * v.nuzz
-        // _x = _x + cos(_dir) * length//* (i+1) 
-        // _y = _y + sin(_dir) * length//* (i+1) 
+        i++
+        _dir += (noise(i, frame) - 0.5) * v.nuzz
+        _x = _x + cos(_dir) * length//* (i+1) 
+        _y = _y + sin(_dir) * length//* (i+1) 
         // circle(_x, _y, d)
-        // d -= 1
-    // }
+        backside.push({_x, _y, _d})
+        _d -= 1
+    }
+
+    for (let n = backside.length - 1; n >= 0; n--) {
+        const c = backside[n]
+        circle(c._x, c._y, c._d)
+    }
+    
+    _x = x + 3
+    _y = y
+    _d = d
+    _dir = dir + PI
+    i += 1000
+
+    while (_d > d2) {
+        i++
+        _dir += (noise(i, frame) - 0.5) * v.nuzz
+        _x = _x + cos(_dir) * length//* (i+1) 
+        _y = _y + sin(_dir) * length//* (i+1) 
+        circle(_x, _y, _d)
+        _d -= 1
+    }
 }
 
 function setup() {
