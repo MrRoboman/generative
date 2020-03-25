770ef62a01fa37e8eb2249b6cdbc794e9d9f8ccd
Rob Kayson <rkayson@gmail.com>
2020-03-24 20:59:35 -0700

    Birds eye stack falling over

--- tentacle.js
+++ tentacle.js
@@ -1,59 +1,43 @@
 let frame = 0
+let i
+const rings = []
+let dir = 0
 
 function drawTentacle(x, y, d, d2, seed) {
-    let startX = x
-    let w = d
-    let h = d
-    // stroke(255,165,2, 100)
     strokeWeight(3)
     stroke(254,1,90, 130)
     fill(255)
+    i = 0
+    let _dir = dir
     while (d > d2) {
-        let _x = x
-        let _y = y
-        let n = .5
-        ellipse(_x, _y, w, h)
-        w -= 1
-        h -= 1
-        _x += v._x
-        _y += v._y
-        // const angle = (noise(y * .01, frame)) * (PI*2)
-        // fast left and right
-        const angle = map(noise(y * .01, frame, seed) - 0.5, -0.05, 0.05, 0, 2*PI)
-        // slow boring
-        // const angle = map(noise(y * .01, frame) - 0.5, -0.2, 0.2, -PI*3/4, -PI/4)
-        x += cos(angle)
-        y -= 3.5
-        // y += sin(angle)
-        d -= .7
-        w = d
-        h = d
+        let ring = rings[i++]
+        if (!ring) {
+            ring = {
+                x,
+                y,
+                d,
+                dir,
+            }
+            rings.push(ring)
+        }
+        const length = 3
+        _dir += noise(i) - 0.5
+        const _x = x + cos(_dir) * (i+1) * length
+        const _y = y + sin(_dir) * (i+1) * length
+        circle(_x, _y, d)
+        d -= 1
     }
-    // fill(0)
-    // ellipse(x + (x < startX ? -3 : 3), y, d / 3, d / 2)
 }
-
-let seed1
-let seed2
 
 function setup() {
     createCanvas(500, 500)
-    seed1 = random(Date.now())
-    seed2 = random(Date.now())
-    seed2 = seed1 + -.3
     v.add('framerate', .002, -3)
-    v.add('_x', 0, -2)
-    v.add('_y', .5, -2)
 }
 
-let t = 0
 function draw() {
     v.input()
     frame += v.framerate
     background(255)
-    drawTentacle(width * .3, height * .8, 20, 1, seed1)
-    drawTentacle(width * .7, height * .8, 20, 1, seed1)
-    drawTentacle(width * .5, height * .75, 80, 1, seed2)
-    // drawTentacle(width * .5, height / 2, min(60, 30 + frame*10), 30, seed2)
-    // noLoop()
+    drawTentacle(width * .5, height * .5, 60, 1)
+    noLoop()
 }
