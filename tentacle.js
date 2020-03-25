924d14bd1a8ff890cd56ae5c0a4400758f34c9df
Rob Kayson <rkayson@gmail.com>
2020-03-24 21:26:37 -0700

    Bottom half worm

--- tentacle.js
+++ tentacle.js
@@ -1,6 +1,6 @@
 let frame = 0
 let i
-const rings = []
+// const rings = []
 let dir = 0
 
 function drawTentacle(x, y, d, d2, seed) {
@@ -10,30 +10,44 @@ function drawTentacle(x, y, d, d2, seed) {
     i = 0
     let _x = x
     let _y = y
+    let _d = d2
     let _dir = dir
-    while (d > d2) {
-        let ring = rings[i++]
-        if (!ring) {
-            ring = {
-                x,
-                y,
-                d,
-                dir,
-            }
-            rings.push(ring)
-        }
-        const length = 3
+    const length = 3
+
+    let backside = []
+    while (_d < d) {
+        i++
         _dir += (noise(i, frame) - 0.5) * v.nuzz
         _x = _x + cos(_dir) * length//* (i+1) 
         _y = _y + sin(_dir) * length//* (i+1) 
-        circle(_x, _y, d)
-        d -= 1
+        circle(_x, _y, _d)
+        _d += 1
     }
+
+    // while (d > d2) {
+        // let ring = rings[i++]
+        // if (!ring) {
+        //     ring = {
+        //         x,
+        //         y,
+        //         d,
+        //         dir,
+        //     }
+        //     rings.push(ring)
+        // }
+
+        // i++
+        // _dir += (noise(i, frame) - 0.5) * v.nuzz
+        // _x = _x + cos(_dir) * length//* (i+1) 
+        // _y = _y + sin(_dir) * length//* (i+1) 
+        // circle(_x, _y, d)
+        // d -= 1
+    // }
 }
 
 function setup() {
     createCanvas(500, 500)
-    v.add('nuzz', 3, -2)
+    v.add('nuzz', 1.7, -2)
     v.add('framerate', .01, -3)
     
 }
