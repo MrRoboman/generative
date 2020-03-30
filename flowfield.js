a5aef09090fb71d264ff4e226fdd3b0117f8d644
Rob Kayson <rkayson@gmail.com>
2020-03-29 20:44:06 -0700

    Eyeball

--- flowfield.js
+++ flowfield.js
@@ -9,6 +9,12 @@ let origin
 
 let particles = []
 
+let eye
+
+function preload() {
+    eye = loadImage('./eye copy.png')
+}
+
 function setup() {
     canvas = createCanvas(500, 500)
 
@@ -20,17 +26,16 @@ function setup() {
 
 
     for (let i = 0; i < particleCount; i++) {
-        particles.push(new Particle(random(width), random(height)))
+        let particle = new Particle(random(width), random(height))
+        // if (particle.pos.dist(origin) < 100) {
+        //     particle.pos 
+        // }
+        particles.push(particle)
     }
 
-    // for (let i = 0; i < particleCount; i++) {
-    //     const particle = new Particle(width/2, height/2)
-    //     particle.vel.x = 1
-    //     particle.vel.rotate(random(TWO_PI))
-    //     particles.push(particle)
-    // }
 }
 
+let f = 0
 function draw() {
     phase1()
     // stroke(0, 80, 13, 10)
@@ -44,6 +49,57 @@ function draw() {
     //     }
     // })
     // particles.forEach(p => p.draw())
+
+    // stroke(0)
+    // noStroke()
+    // fill(200)
+    // const midX = width/2
+    // const midY = height/2
+    // circle(midX, midY, 100)
+
+    // randomSeed(100000)
+    // let points = []
+    // for (let i = 0; i < TWO_PI; i += random(.01, .2)) {
+    //     let r = 36 + (noise(i * 4) - 0.5) * 20
+    //     // r = max(r, 30)
+    //     stroke(0, random(100, 200))
+    //     strokeWeight(random(.5,1.5))
+    //     let point = {x: midX + cos(i) * r, y: midY + sin(i) * r}
+    //     line(midX, midY, point.x, point.y)
+    //     points.push(point)
+    // }
+
+    // stroke(0, 180)
+    // strokeWeight(1)
+    // noStroke()
+    // fill(255, 150)
+    // beginShape()
+    // for (let i = 0; i < points.length; i++) {
+    //     // line(points[i-1].x, points[i-1].y, points[i].x, points[i].y)
+    //     curveVertex(points[i].x, points[i].y)
+    // }
+    // // line(points[points.length-1].x, points[points.length-1].y, points[0].x, points[0].y)
+    // curveVertex(points[0].x, points[0].y)
+    // curveVertex(points[1].x, points[1].y)
+    // curveVertex(points[2].x, points[2].y)
+    // endShape()
+
+    // fill(0)
+    // circle(midX, midY, 23)
+
+    // for (let i = 0; i < particleCount; i++) {
+    //     const particle = new Particle(width/2, height/2)
+    //     particle.vel.x = 1
+    //     particle.vel.rotate(random(TWO_PI))
+    //     particles.push(particle)
+    // }
+
+    image(eye, origin.x - eye.width / 2, origin.y - eye.height / 2)
+    
+    strokeWeight(500)
+    stroke(0, 10)
+    noFill()
+    if (f++ < 65) circle(origin.x, origin.y, 250)
 }
 
 function phase1() {
@@ -60,7 +116,7 @@ function phase1() {
             const vector = createVector(origin.x - vec.x, origin.y - vec.y)
             const dist = vec.dist(origin) * .1
             vector.setMag(dist)
-            vector.rotate((noise(xOff, yOff, zOff) - 0.5) * dist)
+            vector.rotate((noise(xOff, yOff) - 0.5) * dist)
             const angle = vector.heading()
             // vector.setMag(1)
             const index = x + y * cols
@@ -91,7 +147,7 @@ function Particle(x, y) {
     this.maxVel = 2
     this.prevPos = this.pos.copy()
     this.alive = true
-    this.endOfLife = Date.now() + random(2000)
+    this.endOfLife = Date.now() + random(5000)
 
     this.updatePrevPos = () => {
         this.prevPos = this.pos.copy()
@@ -142,6 +198,12 @@ function Particle(x, y) {
         // circle(this.pos.x, this.pos.y, 1)
         if (!this.alive) {
             return
+        }
+        strokeWeight(1)
+        const dist = this.pos.dist(origin)
+        const thresh = 130
+        if (dist > thresh) {
+            strokeWeight((dist - thresh + 2) * .02)
         }
         line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y)
     }
