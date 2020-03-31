d61d740f9ceab99c5ef1af7dd7de977165761f2a
Rob Kayson <rkayson@gmail.com>
2020-03-30 22:47:05 -0700

    flowfield with global variables

--- flowfield.js
+++ flowfield.js
@@ -10,40 +10,63 @@ let particles = []
 
 let layers = []
 
+let seed
+
 function createLayer() {
     const layer = {}
-    layer.xOff = 0.01
-    layer.yOff = 0.01
-    layer.zOff = 0.0001
-    layer.z = 0
-    layer.vectorMag = 0.1
-    layer.scl = 6
-    layer.cols = floor(width / layer.scl)
-    layer.rows = floor(height / layer.scl)
-    layer.angleMax = TWO_PI * 4
+    const num = layers.length.toString()
+    const getName = name => `${num}-${name}`
+    // v.add(getName('xOff'), 0.01, -2) //.5
+    v.add(getName('xOff'), 0.5, -2) //.5
+    // v.add(getName('yOff'), 0.01, -2) // .3
+    v.add(getName('yOff'), 0.3, -2) // .3
+    // v.add(getName('zOff'), 0.0001, -4) // 10.1
+    v.add(getName('zOff'), 10.1, -4) // 10.1
+    v.add(getName('z'), 0)
+    v.add(getName('vectorMag'), 1) // 1
+    v.add(getName('scl'), 6)
+    v.add(getName('particleCount'), 1000)
+    v.add(getName('angleMax'), TWO_PI * 4)
+    layer.cols = floor(width / v[getName('scl')])
+    layer.rows = floor(height / v[getName('scl')])
     layer.flowfield = new Array(layer.cols * layer.rows)
     layer.particles = []
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < v[getName('particleCount')]; i++) {
         layer.particles.push(new Particle(random(width), random(height)))
     }
-    layer.color = [160, 0, 0]
-    layer.alpha = 10
-    layer.strokeWeight = 1
+    v.add(getName('hue'), 160)
+    v.add(getName('saturation'), 50)
+    v.add(getName('brightness'), 50)
+    v.add(getName('alpha'), .05)
+    v.add(getName('strokeWeight'), 1)
 
-    let sclSlider = createSlider(1, floor(width*.25), layer.scl)
-    let sclSpan = createSpan(scl)
-    layer.updateValues = () => {
-        if (sclSlider.value() !== layer.scl) {
-            background(0)
-            layer.scl = sclSlider.value()
-            sclSpan.elt.innerText = layer.scl
-            layer.cols = floor(width / scl)
-            layer.rows = floor(height / scl)
-            layer.flowfield = new Array(layer.cols * layer.rows)
-            layer.particles = []
-            for (let i = 0; i < 1000; i++) {
-                layer.particles.push(new Particle(random(width), random(height)))
-            }
+    // let sclSlider = createSlider(1, floor(width*.25), layer.scl)
+    // let sclSpan = createSpan(scl)
+    // layer.updateValues = () => {
+    //     if (sclSlider.value() !== layer.scl) {
+    //         background(0)
+    //         layer.scl = sclSlider.value()
+    //         sclSpan.elt.innerText = layer.scl
+    //         layer.cols = floor(width / scl)
+    //         layer.rows = floor(height / scl)
+    //         layer.flowfield = new Array(layer.cols * layer.rows)
+    //         layer.particles = []
+    //         for (let i = 0; i < 1000; i++) {
+    //             layer.particles.push(new Particle(random(width), random(height)))
+    //         }
+    //     }
+    // }
+    
+    layer.reset = () => {
+        background(0)
+        randomSeed(seed)
+        noiseSeed(seed)
+        layer.cols = floor(width / v[getName('scl')])
+        layer.rows = floor(height / v[getName('scl')])
+        layer.flowfield = new Array(layer.cols * layer.rows)
+        layer.particles = []
+        for (let i = 0; i < v[getName('particleCount')]; i++) {
+            layer.particles.push(new Particle(random(width), random(height)))
         }
     }
 
@@ -51,12 +74,12 @@ function createLayer() {
         for (let y = 0; y < layer.rows; y++) {
             for (let x = 0; x < layer.cols; x++) {
                 const angle = noise(
-                    x*layer.xOff,
-                    y*layer.yOff,
-                    layer.z*layer.zOff,
-                ) * layer.angleMax
+                    x*v[getName('xOff')],
+                    y*v[getName('yOff')],
+                    v[getName('z')]*v[getName('zOff')],
+                ) * v[getName('angleMax')]
                 const vector = p5.Vector.fromAngle(angle)
-                vector.setMag(layer.vectorMag)
+                vector.setMag(v[getName('vectorMag')])
                 const index = x + y * layer.cols
                 layer.flowfield[index] = vector
             }
@@ -64,16 +87,16 @@ function createLayer() {
     }
     layer.updateParticles = () => {
         layer.particles.forEach(p => {
-            const x = floor(p.pos.x / layer.scl)
-            const y = floor(p.pos.y / layer.scl)
+            const x = floor(p.pos.x / v[getName('scl')])
+            const y = floor(p.pos.y / v[getName('scl')])
             const index = x + y * layer.cols
             const force = layer.flowfield[index]
             p.move(force)
         })
     }
     layer.draw = () => {
-        stroke(...layer.color, layer.alpha)
-        strokeWeight(layer.strokeWeight)
+        stroke(v[getName('hue')],v[getName('saturation')],v[getName('brightness')],v[getName('alpha')])
+        strokeWeight(v[getName('strokeWeight')])
         layer.particles.forEach(p => p.draw())
     }
     
@@ -82,16 +105,33 @@ function createLayer() {
     // Setup Control Panel
 }
 
+function keyPressed() {
+    if (key != 'h' && key != 'l')
+    layers.forEach(layer => layer.reset())
+}
+
 function setup() {
     canvas = createCanvas(500, 500)
+
+    seed = floor(random(10000))
+    // seed = 7565
+    // seed = 3025
+    randomSeed(seed)
+    noiseSeed(seed)
+    createSpan(seed)
+    // v.add('seed', seed)
+
+    colorMode(HSB)
+
     background(0)
 
     createLayer()
 }
 
 function draw() {
+    v.input()
     layers.forEach(layer => {
-        layer.updateValues()
+        // layer.updateValues()
         layer.updateFlowfield()
         layer.updateParticles()
         layer.draw()
