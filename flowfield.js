5e8cd621ab9860179ae4112ff2f007699815a9e0
Rob Kayson <rkayson@gmail.com>
2020-03-30 21:34:16 -0700

    Layers step 1

--- flowfield.js
+++ flowfield.js
@@ -8,85 +8,94 @@ let particleCount = 1500
 
 let particles = []
 
-function setup() {
-    canvas = createCanvas(500, 500)
-    // colorMode(HSB)
-    background(0)
+let layers = []
 
-    cols = floor(width / scl)
-    rows = floor(height / scl)
-    flowfield = new Array(cols * rows)
-    flowfield2 = new Array(cols * rows)
+function createLayer() {
+    const layer = {}
+    layer.xOff = 0.01
+    layer.yOff = 0.01
+    layer.zOff = 0.0001
+    layer.z = 0
+    layer.vectorMag = 0.1
+    layer.scl = 6
+    layer.cols = floor(width / layer.scl)
+    layer.rows = floor(height / layer.scl)
+    layer.angleMax = TWO_PI * 4
+    layer.flowfield = new Array(layer.cols * layer.rows)
+    layer.particles = []
+    for (let i = 0; i < 1000; i++) {
+        layer.particles.push(new Particle(random(width), random(height)))
+    }
+    layer.color = [160, 0, 0]
+    layer.alpha = 10
+    layer.strokeWeight = 1
 
-
-    for (let i = 0; i < particleCount; i++) {
-        const particle = new Particle(random(width), random(height))
-        particle.color = i < 1000 ? [160, 0, 0] : [248, 241, 40]
-        particles.push(particle)
+    let sclSlider = createSlider(1, floor(width*.25), layer.scl)
+    let sclSpan = createSpan(scl)
+    layer.updateValues = () => {
+        if (sclSlider.value() !== layer.scl) {
+            background(0)
+            layer.scl = sclSlider.value()
+            sclSpan.elt.innerText = layer.scl
+            layer.cols = floor(width / scl)
+            layer.rows = floor(height / scl)
+            layer.flowfield = new Array(layer.cols * layer.rows)
+            layer.particles = []
+            for (let i = 0; i < 1000; i++) {
+                layer.particles.push(new Particle(random(width), random(height)))
+            }
+        }
     }
 
-    // stroke(160, 0, 0, 10)
-    // setTimeout(() => {
-    //     stroke(0, 0, 141, 10)
-    //     for (let i = 0; i < particleCount; i++) {
-    //         particles[i].x = random(width)
-    //         particles[i].y = random(height)
-    //     }
-    //     zOff += 1000
-    // }, 10000)
+    layer.updateFlowfield = () => {
+        for (let y = 0; y < layer.rows; y++) {
+            for (let x = 0; x < layer.cols; x++) {
+                const angle = noise(
+                    x*layer.xOff,
+                    y*layer.yOff,
+                    layer.z*layer.zOff,
+                ) * layer.angleMax
+                const vector = p5.Vector.fromAngle(angle)
+                vector.setMag(layer.vectorMag)
+                const index = x + y * layer.cols
+                layer.flowfield[index] = vector
+            }
+        }
+    }
+    layer.updateParticles = () => {
+        layer.particles.forEach(p => {
+            const x = floor(p.pos.x / layer.scl)
+            const y = floor(p.pos.y / layer.scl)
+            const index = x + y * layer.cols
+            const force = layer.flowfield[index]
+            p.move(force)
+        })
+    }
+    layer.draw = () => {
+        stroke(...layer.color, layer.alpha)
+        strokeWeight(layer.strokeWeight)
+        layer.particles.forEach(p => p.draw())
+    }
+    
+    layers.push(layer)
+
+    // Setup Control Panel
+}
+
+function setup() {
+    canvas = createCanvas(500, 500)
+    background(0)
+
+    createLayer()
 }
 
 function draw() {
-    strokeWeight(1)
-    let yOff = 0
-    for (let y = 0; y < rows; y++) {
-        let xOff = 0
-        for (let x = 0; x < cols; x++) {
-            const angle = noise(xOff, yOff, zOff) * TWO_PI * 4
-            const angle2 = noise(xOff, yOff, zOff+1000) * TWO_PI * 4
-            // const angle = createVector(x * scl, y * scl).angleBetween(createVector(width/2, height/2))
-            const vector = p5.Vector.fromAngle(angle)
-            vector.setMag(.1)
-
-            const vector2 = p5.Vector.fromAngle(angle2)
-            vector2.setMag(.1)
-
-            // const origin = createVector(width/2, height/2)
-            // const vec = createVector(x*scl, y*scl)
-            // const vector = createVector(origin.x - vec.x, origin.y - vec.y)
-            // const dist = vec.dist(origin) * .1
-            // vector.setMag(dist)
-            // vector.rotate((noise(xOff, yOff, zOff) - 0.5) * dist)
-            // const angle = vector.heading()
-            // vector.setMag(1)
-            const index = x + y * cols
-            flowfield[index] = vector
-            flowfield2[index] = vector2
-            xOff += inc
-            // push()
-            // stroke(0)
-            // translate(x * scl, y * scl)
-            // rotate(angle)
-            // line(0, 0, scl, 0)
-            // pop()
-        }
-        yOff += inc
-    }
-    zOff += 0.0001
-    particles.forEach((p, i) => {
-        // angle = 2*PI * noise(floor(p.pos.x / scl), floor(p.pos.y / scl))
-        // p.move(p5.Vector.fromAngle(angle) * 0.01)
-        p.follow(i < 1000 ? flowfield : flowfield2) 
+    layers.forEach(layer => {
+        layer.updateValues()
+        layer.updateFlowfield()
+        layer.updateParticles()
+        layer.draw()
     })
-    particles.forEach(p => {
-        stroke(...p.color, 10)
-        p.draw()
-    })
-    
-    // stroke(0, 80, 13)
-    // strokeWeight(10)
-    // noFill()
-    // circle(250, 250, 20)
     // noLoop()
 }
 
@@ -102,7 +111,7 @@ function Particle(x, y) {
         this.prevPos = this.pos.copy()
     }
 
-    this.follow = (field) => {
+    this.follow = (field, whatsmyindex) => {
         const x = floor(this.pos.x / scl)
         const y = floor(this.pos.y / scl)
         const index = x + y * cols
